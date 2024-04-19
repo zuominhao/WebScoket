6770b175e96652dabd29c17416b5d2ff4f7cc650
zuominhao <zuominhao@qq.com>
2024-04-19 17:13:13 +0800

    2st

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/JR1/minhao/C++/webServer1/web
+# Install script for directory: C:/Users/JR1/minhao/C++/webServer1/WebScoket
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/JR1/minhao/C++/webServer1/web/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/JR1/minhao/C++/webServer1/WebScoket/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
